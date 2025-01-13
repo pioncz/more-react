b40bbaac8c3475ed6d6d9dab75d57660b76dc1fb
Piotr <pioncz@gmail.com>
2025-01-13 20:40:26 +0100

    react router

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Link to="/team">Team</Link>
+    </>
+  )
+};
+
+export default Home;
